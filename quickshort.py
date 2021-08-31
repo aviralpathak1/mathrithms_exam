d509906dc9cc4c363be52ef69424b1c61ed95a0e
aviralpathak1 <83756056+aviralpathak1@users.noreply.github.com>
2021-08-31 13:07:28 +0530

    Update quickshort.py

--- quickshort.py
+++ quickshort.py
@@ -39,44 +39,32 @@ partition (arr[], low, high)                            {partition of the array 
 
 #code for quick short 
 def partition(start, end, array):
-	
-
 	pivot_index = start
 	pivot = array[pivot_index]
-	
-
 	while start < end:
-		
-
+	
 		while start < len(array) and array[start] <= pivot:
 			start += 1
-			
-
+		
 		while array[end] > pivot:
 			end -= 1
-		
-
+	
 		if(start < end):
 			array[start], array[end] = array[end], array[start]
 	
-
 	array[end], array[pivot_index] = array[pivot_index], array[end]
 	
-
 	return end
 	
-
 def quick_sort(start, end, array):
 	
 	if (start < end):
 		
-
 		p = partition(start, end, array)
 
 		quick_sort(start, p - 1, array)
 		quick_sort(p + 1, end, array)
 		
-
 array = [ 83 , 1 , 45 , 95 , 45 , 52 , 11 , 470 , 45 , 67 , 82 ]
 quick_sort(0, len(array) - 1, array)
 
